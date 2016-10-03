b0ea5ce8ff8a028654a67b7af1bfecf4aa8e6cc6
orion46 <earth.nobu.light@gmail.com>
2016-10-03 16:05:25 +0900

    Implement subtract

--- calc.py
+++ calc.py
@@ -9,9 +9,14 @@ def pow(x,y):
     else:
         return x*pow(x,y-1)
 
+def subtract(x,y):
+    return x-y
+
 class calcTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3,5),8)
+    def test_subtract(self):
+        self.assertEqual(subtract(10,3),7)
     def test_pow(self):
         self.assertEqual(pow(2,4),16)
         self.assertEqual(pow(3,5),243)
