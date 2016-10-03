51aaa47ba74042b3fc17587fe4db1243f8e0e576
orion46 <earth.nobu.light@gmail.com>
2016-10-03 15:10:24 +0900

    Update testcase

--- calc.py
+++ calc.py
@@ -12,6 +12,9 @@ def pow(x,y):
 class calcTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3,5),8)
+    def test_pow(self):
+        self.assertEqual(pow(2,4),16)
+        self.assertEqual(pow(3,5),243)
 
 if __name__ == '__main__':
     unittest.main()
